52badd8cb19fd98eccd54bc43ed6e059996820c1
janderkkotlarski <j.d.kotlarski@gmx.com>
2016-03-31 20:43:03 +0200

    new

---
+++ Dork_Hyperqube_V0-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cassert>
+#include <cmath>
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	const std::string program_name{"Dork Hyperqube V0.1"};
+	assert(program_name != "");
+	
+	
+	
+	return 0;
+}
